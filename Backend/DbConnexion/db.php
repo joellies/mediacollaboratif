f5e695d84ff0605a905dba791473f443641aa777
zaidun <zaidun10@yahoo.com>
2020-08-31 22:40:48 +0200

    insert user(not completed yet)

--- Backend/DbConnexion/db.php
+++ Backend/DbConnexion/db.php
@@ -1,10 +1,10 @@
  <?php
  $servername = "localhost";
- $username = "root";
+ $sqlUserName = "root";
  $password = "";
  
  try {
-   $conn = new PDO("mysql:host=$servername;dbname=meco", $username, $password);
+   $conn = new PDO("mysql:host=$servername;dbname=meco", $sqlUserName, $password);
    // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    echo "Connected successfully";
