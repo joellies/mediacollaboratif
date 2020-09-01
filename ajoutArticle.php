952f69be7e0d1135aab4eb510539a3f3c12632f2
Joéllie Sprumont <joellie@siriusschool.be>
2020-09-01 16:04:28 +0200

    ajout-article-db

--- ajoutArticle.php
+++ ajoutArticle.php
@@ -1,1 +1,27 @@
 <?php
+
+include_once 'connect.php';
+
+//Message success post article
+
+isset($_POST['article_titre']) && isset($_POST['article_contenu']) && isset($_POST['article_image'])) {
+  echo "Votre article a été publié";
+} else echo "Votre article n'a pas été publié";
+
+//Déclaration des variables
+
+$article_titre = $_POST['article_titre'];
+$article_contenu = $_POST['article_contenu'];
+$article_image = $_POST['article_image'];
+
+//Insertion données dans DB
+
+$sql = 'INSERT INTO articles(article_titre, article_contenu, article_image) VALUES(:article_titre, :article_contenu, :article_image)';
+
+//Prepare
+
+$statement = $conn->prepare($sql);
+
+if ($statement->execute([':article_titre' => $article_titre, ':article_contenu' => $article_contenu, ':article_image' => $article_image])) {
+  echo 'Ok';
+}
---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$dbname = "meco";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername; dbname=meco", $username, $password);
+
+  //On définit le mode d'erreur de PDO sur Exception
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'Connexion réussie';
+}
+/*On capture les exceptions si une exception est lancée et on affiche les informations relatives à celle-ci*/ catch (PDOException $e) {
+  echo "Erreur : " . $e->getMessage();
+}
