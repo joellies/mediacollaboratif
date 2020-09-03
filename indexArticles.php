f37ca2f9f4cac9eb18279be39d6ecdaac6012ebe
Joéllie Sprumont <joellie@siriusschool.be>
2020-09-03 16:01:44 +0200

    #feat-read-article-style-start

--- indexArticles.php
+++ indexArticles.php
@@ -14,28 +14,50 @@ $articles = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<div class="indexArticles">
-  <table class="listeArticles">
-    <tr>
-      <th>Image</th>
-      <th>Titre</th>
-      <th>Contenu</th>
-      <th>Date</th>
-      <th>Actions</th>
-    </tr>
-    <?php foreach ($articles as $value) {
-    ?>
-    <tr>
-      <td><?= $value['article_image']; ?></td>
-      <td><?= $value['article_titre']; ?></td>
-      <td><?= $value['article_contenu']; ?></td>
-      <td><?= $value['article_date']; ?></td>
-      <td><a href="">Afficher</a><a href="">Modifier</a><a href="">Supprimer</a>
-      </td>
-    </tr>
-    <?php
-    };
-    ?>
+<!DOCTYPE html>
+<html lang="fr">
 
-  </table>
-</div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Index articles</title>
+  <link rel="stylesheet" type="text/css" href="indexArticles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+  <div class="menu">
+    <a href=""><img src="/images/home-icon.png"></a>
+    <a href=""><img src="/images/rechercher-icone.png"></a>
+    <a href=""><img src="/images plus-icon.png"></a>
+    <a href=""><img src="/images/profile-icon.png"> </a>
+  </div>
+  <h1>Liste des articles</h1>
+  <?php foreach ($articles as $value) {
+  ?>
+
+  <div class=" listArticles">
+
+    <div class="leftContent">
+      <div class="articleImage"><?= $value['article_image']; ?></div>
+    </div>
+    <div class="rightContent">
+      <div class=" articleTitre"><?= $value['article_titre']; ?></div>
+      <div class="articleDate"><?= $value['article_date']; ?></div>
+      <div class="btnChangeArticle">
+        <div class="btnModifier"><a href="">Modifier</a></div>
+        <div class="btnSupprimer"><a href="">Supprimer</a></div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <?php
+  };
+  ?>
+  </div>
+</body>
+
+</html>
+
+</html>
